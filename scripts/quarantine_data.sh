da87a0539e2e8b6f858e0bd280aac20803d2f8ed
Raymond Finzel <finze006@umn.edu>
2019-01-04 15:08:56 -0600

    New version

--- scripts/quarantine_data.sh
+++ scripts/quarantine_data.sh
@@ -1,16 +1,23 @@
 temp=$(mktemp "${TMPDIR:-/tmp/}$(basename $0).XXXX")
 
 {
-    grep -P -nl -- '\d+/\d+-' "$@"
-    grep -nl '[^[:print:]]' "$@"
-    grep -P -nl '[^\x00-\x7F]' "$@"
-    grep -nl $'\t' "$@"
-    pcregrep -Mnl  '^$.*\n^\.' "$@"
+    ggrep -P -lr -- '\d+/\d+-' "$@"      # e.g. "45/53-"
+    ggrep -P -lr -- '\.+\S/\S\s+' "$@"   # e.g. ".H/O "
+    ggrep -lr -- '[^[:print:]]' "$@"     # i.e. control codes
+    ggrep -P -lr -- '[^\x00-\x7F]' "$@"  # i.e. unicode
+    ggrep -lr -- $'\t' "$@"              # i.e. tabs
+    pcregrep -Mlr -- '^$.*\n^\.' "$@"    # e.g. "\n. "
+    ggrep -P -lr -- '\s+\.+\s+' "$@"     # e.g. " . "
 } >> "$temp"
 
-mkdir -p $DATA_DIRECTORY/quarantine
+mkdir -p quarantine
 
 while read -r filename
 do
-    mv -- "$filename" $DATA_DIRECTORY/quarantine
+    mv -- "$filename" quarantine
 done < "$temp"
+
+# Usage:
+#       ./quarantine_data.sh <directories to quarantine>
+# Example:
+#       ./quarantine_data.sh data_in
