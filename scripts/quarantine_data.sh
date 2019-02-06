53bdd6ecebc8ac80f4b368e3ac102866a6e40b84
Raymond Finzel <finze006@umn.edu>
2019-02-06 11:50:47 -0600

    newest version of quarantine script

--- scripts/quarantine_data.sh
+++ scripts/quarantine_data.sh
@@ -1,13 +1,16 @@
 temp=$(mktemp "${TMPDIR:-/tmp/}$(basename $0).XXXX")
 
 {
-    ggrep -P -lr -- '\d+/\d+-' "$@"      # e.g. "45/53-"
-    ggrep -P -lr -- '\.+\S/\S\s+' "$@"   # e.g. ".H/O "
-    ggrep -lr -- '[^[:print:]]' "$@"     # i.e. control codes
-    ggrep -P -lr -- '[^\x00-\x7F]' "$@"  # i.e. unicode
-    ggrep -lr -- $'\t' "$@"              # i.e. tabs
-    pcregrep -Mlr -- '^$.*\n^\.' "$@"    # e.g. "\n. "
-    ggrep -P -lr -- '\s+\.+\s+' "$@"     # e.g. " . "
+                                         # Offenders:                                     # Mitigation:
+    ggrep -P -lr -- '\d+/\d+-' "$@"      # e.g. "45/53-"                                  # replace "-" with " "
+    ggrep -P -lr -- '\.+\S/\S\s+' "$@"   # e.g. ".H/O "                                   # replace "." with " "
+    ggrep -lr -- '[^[:print:]]' "$@"     # i.e. control codes                             # delete matched characters
+    ggrep -P -lr -- '[^\x00-\x7F]' "$@"  # i.e. unicode                                   # replace characters with closest ascii
+    ggrep -lr -- $'\t' "$@"              # i.e. tabs                                      # replace tab with " "
+    pcregrep -Mlr -- '^$.*\n^\.' "$@"    # e.g. "\n. "                                    # replace "." with " "
+    ggrep -P -lr -- '\s+\.+\s+' "$@"     # e.g. " . "                                     # replace "." with " "
+    ggrep -P -lr -- '^\.+' "$@"          # i.e. period alone at the beginning of a line   # replace "." with " "
+    # TODO                               # i.e. pipes                                     # replace "|" with " "
 } >> "$temp"
 
 mkdir -p quarantine
